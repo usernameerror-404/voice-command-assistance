56172c775814b2a6665c408f6af8beccef6e8cfb
usernameerror-404 <yateshsharma485@gmail.com>
2024-11-12 18:35:36 +0530

    1musicLibrary.py

--- musicLibrary.py
+++ musicLibrary.py
@@ -9,5 +9,125 @@ music = {
     "paro" : "https://www.youtube.com/watch?v=B-99Pm--78Y",
 
     "interlinked": "https://www.youtube.com/watch?v=aKCi0nY2Ouw"
+
+     "blinding lights": "https://www.youtube.com/watch?v=4NRXx6U8ABQ",
+
+     "save your tears": "https://www.youtube.com/watch?v=XXYlFuWEuKI",
+
+     "levitating": "https://www.youtube.com/watch?v=TUVcZfQe-Kw",
+     
+     "bad guy": "https://www.youtube.com/watch?v=DyDfgMOUjCI",
+     
+     "industry baby": "https://www.youtube.com/watch?v=UTHLKHL_whs",
+     
+     "smooth criminal": "https://www.youtube.com/watch?v=h_D3VFfhvs4",
+     
+     "heat waves": "https://www.youtube.com/watch?v=mRD0-GxqHVo"
+
+     "shape of you": "https://www.youtube.com/watch?v=JGwWNGJdvx8",
+
+     "thinking out loud": "https://www.youtube.com/watch?v=lp-EO5I60KA",
+     
+     "uptown funk": "https://www.youtube.com/watch?v=OPf0YbXqDm0",
+     
+     "despacito": "https://www.youtube.com/watch?v=kJQP7kiw5Fk",
+     
+     "senorita": "https://www.youtube.com/watch?v=Pkh8UtuejGw",
+     
+     "perfect": "https://www.youtube.com/watch?v=2Vv-BfVoq4g",
+     
+     "bad habits": "https://www.youtube.com/watch?v=orJSJGHjBLI",
+     
+     "rockstar": "https://www.youtube.com/watch?v=UceaB4D0jpo",
+     
+     "sunflower": "https://www.youtube.com/watch?v=ApXoWvfEYVU",
+     
+     "havana": "https://www.youtube.com/watch?v=HCjNJDNzw8Y",
+     
+     "butter": "https://www.youtube.com/watch?v=WMweEpGlu_U",
+     
+     "dynamite": "https://www.youtube.com/watch?v=gdZLi9oWNZg",
+     
+     "faded": "https://www.youtube.com/watch?v=60ItHLz5WEA",
+     
+     "believer": "https://www.youtube.com/watch?v=7wtfhZwyrcc",
+     
+     "thunder": "https://www.youtube.com/watch?v=fKopy74weus",
+     
+     "radioactive": "https://www.youtube.com/watch?v=ktvTqknDobU",
+     
+     "rolling in the deep": "https://www.youtube.com/watch?v=rYEDA3JcQqw",
+     
+     "hello": "https://www.youtube.com/watch?v=YQHsXMglC9A",
+     
+     "someone like you": "https://www.youtube.com/watch?v=hLQl3WQQoQ0",
+     
+     "watermelon sugar": "https://www.youtube.com/watch?v=E07s5ZYygMg",
+     
+     "as it was": "https://www.youtube.com/watch?v=H5v3kku4y6Q",
+     
+     "stressed out": "https://www.youtube.com/watch?v=pXRviuL6vMY",
+     
+     "heathens": "https://www.youtube.com/watch?v=UprcpdwuwCg",
+     
+     "shallow": "https://www.youtube.com/watch?v=bo_efYhYU2A",
+     
+     "can't stop the feeling": "https://www.youtube.com/watch?v=ru0K8uYEZWw",
+     
+     "love yourself": "https://www.youtube.com/watch?v=oyEuk8j8imI",
+     
+     "stay": "https://www.youtube.com/watch?v=kTJczUoc26U",
+     
+     "goosebumps": "https://www.youtube.com/watch?v=Dst9gZkq1a8",
+     
+     "sicko mode": "https://www.youtube.com/watch?v=6ONRf7h3Mdk",
+     
+     "lucid dreams": "https://www.youtube.com/watch?v=mzB1VGEGcSU",
+     
+     "all of me": "https://www.youtube.com/watch?v=450p7goxZqg",
+     
+     "let her go": "https://www.youtube.com/watch?v=RBumgq5yVrA",
+     
+     "clocks": "https://www.youtube.com/watch?v=d020hcWA_Wg",
+     
+     "yellow": "https://www.youtube.com/watch?v=yKNxeF4KMsY",
+     
+     "paradise": "https://www.youtube.com/watch?v=1G4isv_Fylg",
+     
+     "the scientist": "https://www.youtube.com/watch?v=RB-RcX5DS5A",
+     
+     "viva la vida": "https://www.youtube.com/watch?v=dvgZkm1xWPE",
+     
+     "on my way": "https://www.youtube.com/watch?v=dhYOPzcsbGM",
+     
+     "ignite": "https://www.youtube.com/watch?v=Az-mGR-CehY",
+     
+     "taki taki": "https://www.youtube.com/watch?v=ixkoVwKQaJg",
+     
+     "fancy like": "https://www.youtube.com/watch?v=asjC73wHnZo",
+     
+     "girls like you": "https://www.youtube.com/watch?v=aJOTlE1K90k",
+     
+     "sugar": "https://www.youtube.com/watch?v=09R8_2nJtjg",
+     
+     "maps": "https://www.youtube.com/watch?v=NmugSMBh_iI",
+     
+     "animals": "https://www.youtube.com/watch?v=qpgTC9MDx1o",
+     
+     "locked out of heaven": "https://www.youtube.com/watch?v=e-fA-gBCkj0",
+     
+     "just the way you are": "https://www.youtube.com/watch?v=LjhCEhWiKXk",
+     
+     "grenade": "https://www.youtube.com/watch?v=SR6iYWJxHqs",
+     
+     "counting stars": "https://www.youtube.com/watch?v=hT_nvWreIhg",
+     
+     "apologize": "https://www.youtube.com/watch?v=ZSM3w1v-A_Y",
+     
+     "secrets": "https://www.youtube.com/watch?v=qHm9MG9xw1o",
+     
+     "hall of fame": "https://www.youtube.com/watch?v=mk48xRzuNvA"
+
+
         
         }
