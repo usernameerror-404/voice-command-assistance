814144aaa197fc8697fa32887dd41dbf07975ab9
usernameerror-404 <yateshsharma485@gmail.com>
2024-11-12 18:41:29 +0530

    Update musicLibrary.py

--- musicLibrary.py
+++ musicLibrary.py
@@ -127,7 +127,192 @@ music = {
      "secrets": "https://www.youtube.com/watch?v=qHm9MG9xw1o",
      
      "hall of fame": "https://www.youtube.com/watch?v=mk48xRzuNvA"
-#you can add more songs
+
+     "we don't talk anymore": "https://www.youtube.com/watch?v=3AtDnEC4zak",
+
+     "attention": "https://www.youtube.com/watch?v=nfs8NYg7yQM",
+     
+     "see you again": "https://www.youtube.com/watch?v=RgKAFK5djSk",
+     
+     "one call away": "https://www.youtube.com/watch?v=BxuY9FET9Y4",
+     
+     "how long": "https://www.youtube.com/watch?v=CwfoyVa980U",
+     
+     "closer": "https://www.youtube.com/watch?v=PT2_F-1esPk",
+     
+     "something just like this": "https://www.youtube.com/watch?v=FM7MFYoylVs",
+     
+     "roses": "https://www.youtube.com/watch?v=G5Mv2iV0wkU",
+     
+     "don't let me down": "https://www.youtube.com/watch?v=Io0fBr1XBUA",
+     
+     "paris": "https://www.youtube.com/watch?v=RhU9MZ98jxo",
+     
+     "middle": "https://www.youtube.com/watch?v=IJNR2EpS0jw",
+     
+     "you know you like it": "https://www.youtube.com/watch?v=aBn7bjy9c4U",
+     
+     "lean on": "https://www.youtube.com/watch?v=YqeW9_5kURI",
+     
+     "turn down for what": "https://www.youtube.com/watch?v=HMUDVMiITOU",
+     
+     "hey brother": "https://www.youtube.com/watch?v=6Cp6mKbRTQY",
+     
+     "wake me up": "https://www.youtube.com/watch?v=IcrbM1l_BoI",
+     
+     "levels": "https://www.youtube.com/watch?v=_ovdm2yX4MA",
+     
+     "the nights": "https://www.youtube.com/watch?v=UtF6Jej8yb4",
+     
+     "waiting for love": "https://www.youtube.com/watch?v=cHHLHGNpCSA",
+     
+     "titanium": "https://www.youtube.com/watch?v=JRfuAukYTKg",
+     
+     "she wolf": "https://www.youtube.com/watch?v=PVzljDmoPVs",
+     
+     "latch": "https://www.youtube.com/watch?v=93ASUImTedo",
+     
+     "stay with me": "https://www.youtube.com/watch?v=pB-5XG-DbAA",
+     
+     "too good at goodbyes": "https://www.youtube.com/watch?v=J_ub7Etch2U",
+     
+     "i'm not the only one": "https://www.youtube.com/watch?v=nCkpzqqog4k",
+     
+     "writing's on the wall": "https://www.youtube.com/watch?v=8jzDnsjYv9A",
+     
+     "diamonds": "https://www.youtube.com/watch?v=lWA2pjMjpBs",
+     
+     "we found love": "https://www.youtube.com/watch?v=tg00YEETFzg",
+     
+     "stay": "https://www.youtube.com/watch?v=JF8BRvqGCNs",
+     
+     "needed me": "https://www.youtube.com/watch?v=wfN4PVaOU5Q",
+     
+     "work": "https://www.youtube.com/watch?v=HL1UzIK-flA",
+     
+     "where have you been": "https://www.youtube.com/watch?v=HBxt_v0WF6Y",
+     
+     "only girl": "https://www.youtube.com/watch?v=pa14VNsdSYM",
+     
+     "umbrella": "https://www.youtube.com/watch?v=CvBfHwUxHIk",
+     
+     "rude boy": "https://www.youtube.com/watch?v=e82VE8UtW8A",
+     
+     "wild thoughts": "https://www.youtube.com/watch?v=fyaI4-5849w",
+     
+     "hotline bling": "https://www.youtube.com/watch?v=uxpDa-c-4Mc",
+     
+     "god's plan": "https://www.youtube.com/watch?v=xpVfcZ0ZcFM",
+     
+     "nice for what": "https://www.youtube.com/watch?v=U9BwWKXjVaI",
+     
+     "in my feelings": "https://www.youtube.com/watch?v=DRS_PpOrUZ4",
+     
+     "nonstop": "https://www.youtube.com/watch?v=zrFI2gJSuwA",
+     
+     "sicko mode": "https://www.youtube.com/watch?v=3CxtK7-XtE0",
+     
+     "antidote": "https://www.youtube.com/watch?v=KnZ8h3MRuYg",
+     
+     "highest in the room": "https://www.youtube.com/watch?v=tp2dRbB8q9U",
+     
+     "stargazing": "https://www.youtube.com/watch?v=rxrzU1KrjUc",
+     
+     "goosebumps": "https://www.youtube.com/watch?v=khKt5xoej4M",
+     
+     "pick up the phone": "https://www.youtube.com/watch?v=-LPXWAZp8zs",
+     
+     "butterfly effect": "https://www.youtube.com/watch?v=_EyZUTDAH0U",
+     
+     "circles": "https://www.youtube.com/watch?v=wXhTHyIgQ_U",
+     
+     "sunflower": "https://www.youtube.com/watch?v=ApXoWvfEYVU",
+     
+     "wow": "https://www.youtube.com/watch?v=279qZRqKuBo",
+     
+     "goodbyes": "https://www.youtube.com/watch?v=ba7mB8oueCY",
+     
+     "rockstar": "https://www.youtube.com/watch?v=UceaB4D0jpo",
+     
+     "better now": "https://www.youtube.com/watch?v=UYwF-jdcVjY",
+     
+     "congratulations": "https://www.youtube.com/watch?v=SC4xMk98Pdc",
+     
+     "psycho": "https://www.youtube.com/watch?v=au2n7VVGv_c",
+     
+     "white iverson": "https://www.youtube.com/watch?v=SLsTskih7_I",
+     
+     "money": "https://www.youtube.com/watch?v=zUOh09GoQgk",
+     
+     "bodak yellow": "https://www.youtube.com/watch?v=PEGccV-NOm8",
+     
+     "i like it": "https://www.youtube.com/watch?v=xTlNMmZKwpA",
+     
+     "bartier cardi": "https://www.youtube.com/watch?v=xRlQh2P2z7Y",
+     
+     "wap": "https://www.youtube.com/watch?v=hsm4poTWjMs",
+     
+     "up": "https://www.youtube.com/watch?v=rCiBgLOcuKU",
+     
+     "drip": "https://www.youtube.com/watch?v=tyU1uMz7wnA",
+     
+     "finesse": "https://www.youtube.com/watch?v=LsoLEjrDogU",
+     
+     "24k magic": "https://www.youtube.com/watch?v=UqyT8IEBkvY",
+     
+     "that's what i like": "https://www.youtube.com/watch?v=PMivT7MJ41M",
+     
+     "just the way you are": "https://www.youtube.com/watch?v=LjhCEhWiKXk",
+     
+     "talking to the moon": "https://www.youtube.com/watch?v=DxNGVPDFcJQ",
+     
+     "locked out of heaven": "https://www.youtube.com/watch?v=e-fA-gBCkj0",
+     
+     "grenade": "https://www.youtube.com/watch?v=SR6iYWJxHqs",
+     
+     "treasure": "https://www.youtube.com/watch?v=nPvuNsRccVw",
+     
+     "marry you": "https://www.youtube.com/watch?v=8lNQXQ5aOlo",
+     
+     "billionaire": "https://www.youtube.com/watch?v=8j741TUIET0",
+     
+     "versace on the floor": "https://www.youtube.com/watch?v=SK3aDJ2IRJg",
+     
+     "lazy song": "https://www.youtube.com/watch?v=fLexgOxsZu0",
+     
+     "young dumb & broke": "https://www.youtube.com/watch?v=IPfJnp1guPc",
+     
+     "location": "https://www.youtube.com/watch?v=by3yRdlQvzs",
+     
+     "better": "https://www.youtube.com/watch?v=0XkYRdsNwYg",
+     
+     "talk": "https://www.youtube.com/watch?v=hE2Ira-Cwxo",
+     
+     "love lies": "https://www.youtube.com/watch?v=BIbZPzGxqPY",
+     
+     "silence": "https://www.youtube.com/watch?v=Tx1sqYc3qas",
+     
+     "wolves": "https://www.youtube.com/watch?v=cH4E_t3m3xM",
+     
+     "friends": "https://www.youtube.com/watch?v=jzD_yyEcp0M",
+     
+     "happier": "https://www.youtube.com/watch?v=m7Bc3pLyij0",
+     
+     "alone": "https://www.youtube.com/watch?v=1-xGerv5FOk",
+     
+     "lost in japan": "https://www.youtube.com/watch?v=Hy9o2HjJ6aU",
+     
+     "there's nothing holdin' me back": "https://www.youtube.com/watch?v=dT2owtxkU8k",
+     
+     "treat you better": "https://www.youtube.com/watch?v=lY2yjAdbvdQ",
+     
+     "mercy": "https://www.youtube.com/watch?v=KkGVmN68ByU",
+     
+     "stitches": "https://www.youtube.com/watch?v=VbfpW0pbvaU",
+     
+     "if i can't have you": "https://www.youtube.com/watch?v=oTJ-oqwxdZY"
+
+
 
         
         }
